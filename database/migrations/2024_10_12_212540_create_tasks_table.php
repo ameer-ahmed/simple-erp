db54cf90d64bbfb87fa43e1e38019979b34ab77b
Ameer Ahmed <31576021+ameer-ahmed@users.noreply.github.com>
2024-10-14 05:11:06 +0300

    feat: add task management functionality
    Adds the ability for managers to create, assign, and manage tasks for employees.  Includes a task status enum for tracking progress.

--- database/migrations/2024_10_12_212540_create_tasks_table.php
+++ database/migrations/2024_10_12_212540_create_tasks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Enums\TaskStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->enum('status', TaskStatus::values());
             $table->foreignId('manager_id')->constrained()->restrictOnDelete()->cascadeOnUpdate();
             $table->foreignId('employee_id')->constrained()->restrictOnDelete()->cascadeOnUpdate();
             $table->timestamps();
--- resources/views/dashboard/core/includes/sidebar.blade.php
+++ resources/views/dashboard/core/includes/sidebar.blade.php
@@ -1,19 +1,21 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ route('/') }}" class="brand-link">
+    <a href="{{ route(session('guard').'./') }}" class="brand-link">
         {{--        <img src="{{asset("logo.png")}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-        <span class="brand-text font-weight-light">@lang('dashboard.Elryad')</span>
+        <span class="brand-text font-weight-light">@lang('dashboard.Simple ERP')</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ auth()->user()->image? asset(auth()->user()->image) :asset('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img
+                    src="{{ auth(session('guard'))->user()->image? asset(auth(session('guard'))->user()->image) :asset('img/user2-160x160.jpg') }}"
+                    class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+                <a href="#" class="d-block">{{ auth(session('guard'))->user()->name }}</a>
             </div>
         </div>
 
@@ -21,22 +23,57 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
-                <li class="nav-item  {{ in_array(request()->route()->getName(),['/'])? 'menu-open': '' }}">
-                    <a href="{{ url('/') }}" class="nav-link">
+                <li class="nav-item  {{ in_array(request()->route()->getName(),['manager./', 'employee./'])? 'menu-open': '' }}">
+                    <a href="{{ route(session('guard').'./') }}" class="nav-link">
                         <i class="nav-icon fas fa-circle"></i>
                         <p>
                             @lang('dashboard.Home')
                         </p>
                     </a>
                 </li>
-                <li class="nav-item  {{ in_array(request()->route()->getName(),['users.index', 'users.create', 'users.edit', 'users.show'])? 'menu-open': '' }}">
-                    <a href="{{ route('users.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
-                        <p>
-                            @lang('dashboard.users')
-                        </p>
-                    </a>
-                </li>
+
+                @if(session('guard') == 'manager' && !auth('manager')->user()->is_stakeholder)
+                    <li class="nav-item  {{ in_array(request()->route()->getName(),['manager.employees.index', 'manager.employees.create', 'manager.employees.edit'])? 'menu-open': '' }}">
+                        <a href="{{ route('manager.employees.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                @lang('dashboard.Employees')
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item  {{ in_array(request()->route()->getName(),['manager.tasks.index', 'manager.tasks.create', 'manager.tasks.edit'])? 'menu-open': '' }}">
+                        <a href="{{ route('manager.tasks.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                @lang('dashboard.Tasks')
+                            </p>
+                        </a>
+                    </li>
+                @endif
+
+                @if(session('guard') == 'manager' && auth('manager')->user()->is_stakeholder)
+                    <li class="nav-item  {{ in_array(request()->route()->getName(),['manager.departments.index', 'manager.departments.create', 'manager.departments.edit'])? 'menu-open': '' }}">
+                        <a href="{{ route('manager.departments.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                @lang('dashboard.Departments')
+                            </p>
+                        </a>
+                    </li>
+                @endif
+
+                @if(session('guard') == 'employee')
+                    <li class="nav-item  {{ in_array(request()->route()->getName(),['employee.tasks.index'])? 'menu-open': '' }}">
+                        <a href="{{ route('employee.tasks.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                @lang('dashboard.Tasks')
+                            </p>
+                        </a>
+                    </li>
+                @endif
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
